4520d5de1951a173543a3bae1e88e2d36aa0734f
Jan Zisenis <jan.zisenis@gmail.com>
2025-04-04 07:54:57 +0200

    added packages

--- flake.nix
+++ flake.nix
@@ -1,47 +1,49 @@
 {
   description = "Get Started with LaTeX";
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
-    with flake-utils.lib; eachSystem allSystems
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          tex = pkgs.texlive.combine {
-            inherit (pkgs.texlive) 
-              scheme-full
-              latex-bin
-              latexmk;
+    with flake-utils.lib;
+    eachSystem allSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-full latex-bin latexmk collection-latex
+            collection-fontsrecommended collection-latexextra;
+        };
+      in rec {
+        documentName = "Zisenis_CV";
+        packages = {
+          document = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "latex-document";
+            src = self;
+            buildInputs = [ pkgs.coreutils pkgs.latex2html tex ];
+            phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+            buildPhase = ''
+              export PATH="${pkgs.lib.makeBinPath buildInputs}";
+              mkdir -p .cache/texmf-var
+              env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                SOURCE_DATE_EPOCH=$(date +%s) \
+                OSFONTDIR=${pkgs.commit-mono}/share/fonts \
+                latexmk -interaction=nonstopmode -pdf -lualatex \
+                ${documentName}.tex; \
+                latex2html -noinfo ${documentName}.tex
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp ${documentName}.pdf $out/
+              cp -r ${documentName} $out/
+            '';
           };
-        in
-        rec {
-          documentName = "Zisenis_CV";
-          packages = {
-            document = pkgs.stdenvNoCC.mkDerivation rec {
-              name = "latex-document";
-              src = self;
-              buildInputs = [ pkgs.coreutils pkgs.latex2html tex ];
-              phases = [ "unpackPhase" "buildPhase" "installPhase" ];
-              buildPhase = ''
-                export PATH="${pkgs.lib.makeBinPath buildInputs}";
-                mkdir -p .cache/texmf-var
-                env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-                  SOURCE_DATE_EPOCH=$(date +%s) \
-                  OSFONTDIR=${pkgs.commit-mono}/share/fonts \
-                  latexmk -interaction=nonstopmode -pdf -lualatex \
-                  ${documentName}.tex; \
-                  latex2html -noinfo ${documentName}.tex
-              '';
-              installPhase = ''
-                mkdir -p $out
-                cp ${documentName}.pdf $out/
-                cp -r ${documentName} $out/
-              '';
-            };
+        };
+        packages.default = packages.document;
+        devShell = with pkgs;
+          mkShell {
+            packages = [ packages.document.buildInputs ];
+            shellHook = "echo -n Hello LaTeX";
           };
-          packages.default = packages.document;
-          devShell = with pkgs; mkShell { packages = [ packages.document.buildInputs ]; shellHook = ''echo -n Hello LaTeX''; };
-    });
+      });
 }
